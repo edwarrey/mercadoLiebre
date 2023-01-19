f4a0336206928caced33296a72b96bc8ad28df89
edwar reyes <reyedwar.92@gmail.com>
2023-01-18 19:35:27 -0500

    se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'));
+
+
+app.listen(3000, () => {
+    console.log('servidor funcionando');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+})
